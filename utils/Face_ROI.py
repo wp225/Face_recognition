b1829b00b0ce8c3768a7d4740a8d9d7ac1d43d9d
jeorgejoshi <85611495+wp225@users.noreply.github.com>
2024-01-11 14:20:22 +0545

    optimized training top implemented VVG_FACE

--- utils/Face_ROI.py
+++ utils/Face_ROI.py
@@ -1,13 +1,13 @@
 import cv2
 
-def Harr_Cascase(frame):
+def Face_Capture(frame):
     '''
     :param frame: Input frame only containing one subject
     :return: ROI of face
     '''
     face_cascade = cv2.CascadeClassifier('../haarcascade_frontalface_default.xml')
     frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    frame_gray = cv2.equalizeHist(frame_gray)
+    # frame_gray = cv2.equalizeHist(frame_gray)
 
     # Detect faces
     faces = face_cascade.detectMultiScale(frame_gray, scaleFactor=1.1, minNeighbors=9)
@@ -18,10 +18,13 @@ def Harr_Cascase(frame):
 
         # Draw a rectangle around the detected face on the original image
         # cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-
+        # cv2.imshow('gege',face_roi)
+        # cv2.waitKey(0)
         return face_roi
+
 
 
 if __name__ == '__main__':
     frame = cv2.imread('../players-of-indian-cricket-team_469389f9b.jpg')
-    extraced_frame = Harr_Cascase(frame)
+    extraced_frame = Face_Capture(frame)
+    print(extraced_frame)
